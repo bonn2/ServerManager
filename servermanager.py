9baf2ec933ebbc16979cdbda04bcda1675595d55
Brett Bonifas <42011672+bonn2@users.noreply.github.com>
2023-04-28 20:07:56 -0400

    Implement better logging

--- servermanager.py
+++ servermanager.py
@@ -1,5 +1,7 @@
 import os.path
 import subprocess
+from threading import Thread
+
 import requests
 from tkinter import *
 from tkinter import messagebox
@@ -11,21 +13,21 @@ paper_builds: dict = {}
 
 
 # Static Methods
-def getProjects() -> list:
-    if os.path.exists("Test Servers"):
-        return os.listdir("Test Servers")
+def get_projects() -> list:
+    if os.path.exists("TestServers"):
+        return os.listdir("TestServers")
     else:
         return []
 
 
-def getProjectVersions(project) -> list:
-    if os.path.exists("Test Servers/" + project):
-        return os.listdir("Test Servers/" + project)
+def get_project_versions(project) -> list:
+    if os.path.exists("TestServers/" + project):
+        return os.listdir("TestServers/" + project)
     else:
         return []
 
 
-def getPaperVersions() -> list:
+def get_paper_versions() -> list:
     global paper_versions
     if not paper_versions == []:
         return paper_versions
@@ -38,7 +40,7 @@ def getPaperVersions() -> list:
     return paper_versions
 
 
-def getPaperBuilds(version: str) -> list:
+def get_paper_builds(version: str) -> list:
     global paper_builds
     if version in paper_builds.keys():
         return paper_builds[version]
@@ -52,19 +54,30 @@ def getPaperBuilds(version: str) -> list:
     return builds
 
 
+def console_reader(f, buffer):
+    while True:
+        line = f.readline()
+        if line:
+            buffer.append(line)
+        else:
+            break
+
+
 class Main:
     # noinspection PyTypeChecker
     def __init__(self):
         # Persistent variables
+        self.reader_thread: Thread = None
+        self.console_buffer: list = []
         self.build_dropdown: OptionMenu = None
         self.version_dropdown: OptionMenu = None
         self.platform_dropdown: OptionMenu = None
         self.new_project_entry: Entry = None
         self.console_entry: Entry = None
-        self.kill_server: Button = None
-        self.stop_server: Button = None
+        self.kill_server_button: Button = None
+        self.stop_server_button: Button = None
         self.console_output: ScrolledText = None
-        self.start_server: Entry = None
+        self.start_server_button: Entry = None
         self.right_frame: Frame = None
         self.left_frame: Frame = None
         self.banner_menu: Menu = None
@@ -75,53 +88,48 @@ class Main:
         self.page: str = ''
         self.project: str = ''
 
-        # Read latest log into previous log content
-        if os.path.exists('testserver/logs/latest.log'):
-            f = open('testserver/logs/latest.log')
-            self.previous_log_content = f.read()
-
         # Create root frame / widget
         self.root = Tk()
         self.root.geometry("800x400")
 
         # Setup root window
         self.root.title("Test")
-        self.root.protocol("WM_DELETE_WINDOW", self.onClosing)
+        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
 
         # Open page
-        self.openSelectProjectPage()
+        self.open_select_project_page()
 
     # Pages
-    def clearPage(self):
+    def clear_page(self):
         for widget in self.root.winfo_children():
             widget.destroy()
 
-    def openSelectProjectPage(self):
-        self.clearPage()
+    def open_select_project_page(self):
+        self.clear_page()
         self.page = "select"
-        if len(getProjects()) == 0:
-            self.openNewProjectPage()
+        if len(get_projects()) == 0:
+            self.open_new_project_page()
             return
-        for project in getProjects():
-            button = Button(self.root, text=project, command=lambda: self.selectProject(project))
+        for project in get_projects():
+            button = Button(self.root, text=project, command=lambda: self.select_project(project))
             button.pack()
-        button = Button(self.root, text="Create New Project", command=self.openProjectPage)
+        button = Button(self.root, text="Create New Project", command=self.open_project_page)
         button.pack()
 
-    def openNewProjectPage(self):
-        self.clearPage()
+    def open_new_project_page(self):
+        self.clear_page()
         self.page = "new"
         label = Label(self.root, text="Enter a project name")
         label.pack(padx=5, pady=5)
         self.new_project_entry = Entry(self.root)
-        self.new_project_entry.bind("<Return>", self.createProject)
+        self.new_project_entry.bind("<Return>", self.create_project)
         self.new_project_entry.pack()
 
-    def openSelectVersionPage(self):
-        self.clearPage()
+    def open_select_version_page(self):
+        self.clear_page()
         self.page = "version"
         if self.project == '':
-            self.openSelectProjectPage()
+            self.open_select_project_page()
             return
         # Left and right division
         self.left_frame = Frame(self.root)
@@ -134,26 +142,27 @@ class Main:
         new_label = Label(self.right_frame, text="Create a version")
         new_label.grid(row=0, column=0)
         self.platform_dropdown = OptionMenu(self.right_frame, StringVar(self.right_frame), "Paper", "Purpur", "Folia",
-                                            command=self.selectPlatform)
+                                            command=self.select_platform)
         self.platform_dropdown.grid(row=1, column=0)
         self.version_dropdown = OptionMenu(self.right_frame, StringVar(self.right_frame), "")
         self.version_dropdown.grid(row=2, column=0)
         self.build_dropdown = OptionMenu(self.right_frame, StringVar(self.right_frame), "")
         self.version_dropdown.grid(row=3, column=0)
 
-    def openProjectPage(self):
-        self.clearPage()
+    def open_project_page(self):
+        self.clear_page()
         self.page = "project"
+        self.logging = False
         if self.project == '':
-            self.openNewProjectPage()
+            self.open_new_project_page()
             return
         self.frame = Frame(self.root)
         self.frame.pack()
 
         # Banner menu
         self.banner_menu = Menu(self.frame)
-        self.banner_menu.add_command(label="Change Project", command=self.openSelectProjectPage)
-        self.banner_menu.add_command(label="Change Version", command=self.openSelectVersionPage)
+        self.banner_menu.add_command(label="Change Project", command=self.open_select_project_page)
+        self.banner_menu.add_command(label="Change Version", command=self.open_select_version_page)
         self.root.config(menu=self.banner_menu)
 
         # Left and right division
@@ -163,73 +172,81 @@ class Main:
         self.right_frame.pack(side=RIGHT, fill=BOTH)
 
         # Buttons
-        self.start_server = Button(self.left_frame, text="Start Server", command=self.startServer)
-        self.start_server.pack(padx=3, pady=3)
-        self.stop_server = Button(self.left_frame, text="Stop Server", command=self.stopServer)
-        self.stop_server.pack(padx=3, pady=3)
-        self.kill_server = Button(self.left_frame, text="Kill Server", command=self.killServer)
-        self.kill_server.pack(padx=3, pady=3)
+        self.start_server_button = Button(self.left_frame, text="Start Server", command=self.start_server)
+        self.start_server_button.pack(padx=3, pady=3)
+        self.stop_server_button = Button(self.left_frame, text="Stop Server", command=self.stop_server)
+        self.stop_server_button.pack(padx=3, pady=3)
+        self.kill_server_button = Button(self.left_frame, text="Kill Server", command=self.kill_server)
+        self.kill_server_button.pack(padx=3, pady=3)
 
         # Console setup
         self.console_output = ScrolledText(self.right_frame)
         self.console_output.config(state=DISABLED)
         self.console_output.grid(row=0, column=0)
         self.console_entry = Entry(self.right_frame)
-        self.console_entry.bind("<Return>", self.sendCommand)
+        self.console_entry.bind("<Return>", self.send_command)
         self.console_entry.grid(row=1, column=0, padx=3, pady=3)
 
+        # Read latest log into previous log content
+        if os.path.exists('TestServers/' + self.project + '/logs/latest.log'):
+            f = open('TestServers/' + self.project + '/logs/latest.log')
+            self.previous_log_content = f.read()
+
     # Project Creation
-    def createProject(self, event):
+    def create_project(self, event):
         self.project = self.new_project_entry.get()
-        if not os.path.exists("Test Servers/" + self.project):
-            os.makedirs("Test Servers/" + self.project, exist_ok=True)
-        self.openProjectPage()
+        if not os.path.exists("TestServers/" + self.project):
+            os.makedirs("TestServers/" + self.project, exist_ok=True)
+        self.open_project_page()
 
-    def selectProject(self, project):
+    def select_project(self, project):
         self.project = project
-        self.openProjectPage()
+        self.open_project_page()
 
-    def selectPlatform(self, selection):
+    def select_platform(self, selection):
         if selection == "Paper":
             self.version_dropdown.destroy()
             default = StringVar(self.right_frame)
-            if not getPaperVersions() == []:
-                default.set(getPaperVersions()[0])
-            self.version_dropdown = OptionMenu(self.right_frame, default, *getPaperVersions(),
-                                               command=self.selectVersion)
+            if not get_paper_versions() == []:
+                default.set(get_paper_versions()[0])
+            self.version_dropdown = OptionMenu(self.right_frame, default, *get_paper_versions(),
+                                               command=self.select_version)
             self.version_dropdown.grid(row=2, column=0)
 
-    def selectVersion(self, selection):
+    def select_version(self, selection):
         self.build_dropdown.destroy()
         default = StringVar(self.right_frame)
-        if not getPaperBuilds(selection) == []:
-            default.set(getPaperBuilds(selection)[0])
-        self.build_dropdown = OptionMenu(self.right_frame, default, *getPaperBuilds(selection))
+        if not get_paper_builds(selection) == []:
+            default.set(get_paper_builds(selection)[0])
+        self.build_dropdown = OptionMenu(self.right_frame, default, *get_paper_builds(selection))
         self.build_dropdown.grid(row=3, column=0)
 
     # Window functions
-    def onClosing(self):
+    def on_closing(self):
         if messagebox.askokcancel("Quit", "Do you want to quit?"):
-            self.killServer()
+            self.kill_server()
             self.root.destroy()
 
     # Widget functions
-    def startServer(self):
+    def start_server(self):
         # Start server process
-        self.server_process = subprocess.Popen('java -jar paper.jar --nogui', cwd="testserver/", stdin=subprocess.PIPE)
+        self.server_process = subprocess.Popen('java -jar paper.jar --nogui', cwd="TestServers/" + self.project,
+                                               stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        self.reader_thread = Thread(target=console_reader, args=(self.server_process.stdout, self.console_buffer))
+        self.reader_thread.daemon = True
+        self.reader_thread.start()
 
-    def stopServer(self):
+    def stop_server(self):
         if self.server_process is not None:
             self.server_process.stdin.write(bytes("stop" + '\r\n', 'ascii'))
-            self.server_process.stdin.flush()
             self.server_process = None
 
-    def killServer(self):
+    def kill_server(self):
         if self.server_process is not None:
             self.server_process.kill()
             self.server_process = None
 
-    def sendCommand(self, event):
+    def send_command(self, event):
         # Only send command and clear entry box if server is running
         if self.server_process is not None:
             command = self.console_entry.get()
@@ -245,18 +262,10 @@ class Main:
             # Check if fully scrolled down before writing to console
             fully_scrolled_down = self.console_output.yview()[1] == 1.0
 
-            # Update console
-            if os.path.exists('testserver/logs/latest.log'):
-                f = open('testserver/logs/latest.log')
-                original_log = f.read()
-                self.log_content = original_log
-                self.log_content = self.log_content.replace(self.previous_log_content, '')
-                if self.log_content != '':
-                    self.console_output.config(state=NORMAL)
-                    self.console_output.insert(INSERT, self.log_content)
-                    self.console_output.config(state=DISABLED)
-                self.previous_log_content = original_log
-                f.close()
+            if self.console_buffer:
+                self.console_output.config(state=NORMAL)
+                self.console_output.insert(END, self.console_buffer.pop(0))
+                self.console_output.config(state=DISABLED)
 
             # Scroll console if it was already scrolled all the way down
             if fully_scrolled_down:
